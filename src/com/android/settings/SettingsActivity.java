e7051364b73fbeda6f88872877ddd78969a6715e
abun880007 <abun880007@team-ub.co.uk>
2015-03-19 09:48:49 +0000

    Team UB settings layout

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1237,7 +1237,7 @@ public class SettingsActivity extends Activity
                             UserManager.DISALLOW_DEBUGGING_FEATURES)) {
                         removeTile = true;
                     }
-                } else if (id == R.id.supersu_settings) {
+                 } else if (id == R.id.supersu_settings) {
                     // Embedding into Settings is supported from SuperSU v1.85 and up
                     boolean supported = false;
                     try {
@@ -1252,6 +1252,17 @@ public class SettingsActivity extends Activity
                     boolean supported = false;
                     try {
                         supported = (getPackageManager().getPackageInfo("com.vipercn.viper4android_v2", 0).versionCode >= 18);
+                    } catch (PackageManager.NameNotFoundException e) {
+ 
+                    }
+                    if (!supported) {
+                        removeTile = true;
+                    }
+                 } else if (id == R.id.audiofx_settings) {
+                    // Embedding into Settings only if app exists (user could manually remove it)
+                    boolean supported = false;
+                    try {
+                        supported = (getPackageManager().getPackageInfo("org.cyanogenmod.audiofx", 0).versionCode >= 18);
                     } catch (PackageManager.NameNotFoundException e) {
  
                     }
